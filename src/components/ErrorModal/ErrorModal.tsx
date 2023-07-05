21ab812ed6606a47638ea95508446023d0b0e9a4
monikaprajapati <monikaprajapati714@gmail.com>
2023-07-05 11:29:26 -0400

    React Validation Popup

--- src/components/ErrorModal/ErrorModal.tsx
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import ReactDOM from "react-dom";
 import classes from "./ErrorModal.module.css";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
@@ -8,26 +9,42 @@ type ErrorModalProps = {
     title: string;
     message: string;
   };
-  onClose: Function
+  onClose: Function;
+};
+
+const Backdrop = (props: any) => {
+  return <div className={classes.backdrop} onClick={props.onClick} />;
+};
+
+const ModalOverlay = (props: any) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.errorText.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.errorText.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button type="button" onClick={props.onClick}>
+          Okay
+        </Button>
+      </footer>
+    </Card>
+  );
 };
 
 const ErrorModal: React.FC<ErrorModalProps> = (props: ErrorModalProps) => {
   return (
     <>
-      <div>
-        <div className={classes.backdrop} />
-        <Card className={classes.modal}>
-          <header className={classes.header}>
-            <h2>{props.errorText.title}</h2>
-          </header>
-          <div className={classes.content}>
-            <p>{props.errorText.message}</p>
-          </div>
-          <footer className={classes.actions}>
-            <Button type="button" onClick={props.onClose}>Okay</Button>
-          </footer>
-        </Card>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay errorText={props.errorText} onClick={props.onClose} />,
+        document.getElementById("backdrop-root")
+      )}
     </>
   );
 };
